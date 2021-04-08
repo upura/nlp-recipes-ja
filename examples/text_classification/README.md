c382227b3db538507b952b8c0e280cc7ba131605
upura <upura0@gmail.com>
2021-04-08 05:01:47 +0000

    Run tfidf

--- examples/text_classification/README.md
+++ examples/text_classification/README.md
@@ -11,8 +11,10 @@ https://github.com/microsoft/nlp-recipes/blob/master/examples/text_classificatio
 
 ## Summary
 
-|Notebook|Environment|Description| 
-|---|---|---|
-|[TF-IDF & Logistic Regression](tfidf_logistic_regression.py)|Local| [Logistic Regression](https://scikit-learn.org/stable/modules/generated/sklearn.linear_model.LogisticRegression.html) with TF-IDF vectors |
-|[TF-IDF & LightGBM](tfidf_lgbm.py)|Local| [LightGBM](https://github.com/microsoft/LightGBM) with TF-IDF vectors |
+|Notebook|Environment|Description|ACC|
+|---|---|---|---|
+|[TF-IDF & Logistic Regression](tfidf_logistic_regression.py)|Local| [Logistic Regression](https://scikit-learn.org/stable/modules/generated/sklearn.linear_model.LogisticRegression.html) with TF-IDF vectors | 0.9308 |
+|[TF-IDF & LightGBM](tfidf_lgbm.py)|Local| [LightGBM](https://github.com/microsoft/LightGBM) with TF-IDF vectors | 0.9559 |
 |[BERT](run_bert.py)|Local| [Transformers BERT](https://github.com/huggingface/transformers) |
+
+Accuracy scores (ACC) are calculated by running code only in fold 0 in the condition that datasets are devided into train/val/test at the rate of 0.6/0.2/0.2.
--- examples/text_classification/tfidf_lgbm.py
+++ examples/text_classification/tfidf_lgbm.py
@@ -1,3 +1,5 @@
+import sys
+
 from konoha import WordTokenizer
 import lightgbm as lgb
 from loguru import logger
@@ -10,6 +12,7 @@ from sklearn.metrics import log_loss
 from sklearn.model_selection import train_test_split, StratifiedKFold
 from tqdm import tqdm
 
+sys.path.append('.')
 from utils_nlp.common.data import Data
 from utils_nlp.dataset.livedoor import load_pandas_df
 from utils_nlp.eval.classification import eval_classification
@@ -52,7 +55,7 @@ def preprocess_data(df):
 
 if __name__ == '__main__':
 
-    df = load_pandas_df(nrows=1000, shuffle=True)
+    df = load_pandas_df(shuffle=True)
     X_train, X_test, y_train, y_test = preprocess_data(df)
 
     RUN_NAME = 'lgbm'
@@ -84,40 +87,40 @@ if __name__ == '__main__':
     }
 
     for fold_id, (train_index, valid_index) in enumerate(tqdm(cv.split(X_train, y_train))):
+        if fold_id == 0:
+            X_tr = X_train.loc[train_index, :]
+            X_val = X_train.loc[valid_index, :]
+            y_tr = y_train[train_index]
+            y_val = y_train[valid_index]
 
-        X_tr = X_train.loc[train_index, :]
-        X_val = X_train.loc[valid_index, :]
-        y_tr = y_train[train_index]
-        y_val = y_train[valid_index]
+            lgb_train = lgb.Dataset(X_tr, y_tr)
+            lgb_eval = lgb.Dataset(X_val, y_val, reference=lgb_train)
 
-        lgb_train = lgb.Dataset(X_tr, y_tr)
-        lgb_eval = lgb.Dataset(X_val, y_val, reference=lgb_train)
+            model = lgb.train(params,
+                            lgb_train,
+                            valid_sets=[lgb_train, lgb_eval],
+                            verbose_eval=10,
+                            num_boost_round=1000,
+                            early_stopping_rounds=10)
 
-        model = lgb.train(params,
-                          lgb_train,
-                          valid_sets=[lgb_train, lgb_eval],
-                          verbose_eval=10,
-                          num_boost_round=1000,
-                          early_stopping_rounds=10)
+            Data.dump(model, f'data/{RUN_NAME}/model_{fold_id}.pkl')
 
-        Data.dump(model, f'data/{RUN_NAME}/model_{fold_id}.pkl')
+            oof_train[valid_index] = model.predict(X_val)
+            score = log_loss(y_val, oof_train[valid_index])
+            logger.info(f'fold {fold_id}, log_loss: {score}')
 
-        oof_train[valid_index] = model.predict(X_val)
-        score = log_loss(y_val, oof_train[valid_index])
-        logger.info(f'fold {fold_id}, log_loss: {score}')
-
-        y_pred = model.predict(X_test)
-        y_preds.append(y_pred)
+            y_pred = model.predict(X_test)
+            y_preds.append(y_pred)
 
     y_preds = np.mean(y_preds, axis=0)
     logger.info(f'test, log_loss: {log_loss(y_test, y_preds)}')
     result_dict = eval_classification(y_test, y_preds.argmax(axis=1))
     logger.info(str(result_dict))
     """
-    {'accuracy': 0.885,
-     'precision': [0.8889, 0.88, 0.8, 0.9333, 0.8261, 0.8, 1.0, 0.9583, 0.9],
-     'recall': [0.8889, 0.88, 0.9091, 0.8235, 0.95, 0.6957, 1.0, 0.8846, 0.9474],
-     'f1': [0.8889, 0.88, 0.8511, 0.875, 0.8837, 0.7442, 1.0, 0.92, 0.9231]}
+    {'accuracy': 0.9559,
+     'precision': [0.9364, 0.9826, 0.9553, 0.9462, 0.9282, 0.8947, 1.0, 0.9886, 0.9677],
+     'recall': [0.931, 0.9713, 0.9884, 0.8627, 0.9655, 0.9053, 1.0, 0.9667, 0.974],
+     'f1': [0.9337, 0.9769, 0.9716, 0.9026, 0.9465, 0.9, 1.0, 0.9775, 0.9709]}
     """
 
     Data.dump(oof_train, f'data/{RUN_NAME}/oof_train.pkl')
