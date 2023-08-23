2904cc1b4d6b3f2344f994fa0ef669b855ac81f5
Naoyg <ab96.naoto@akane.waseda.jp>
2023-08-23 22:15:41 +0900

    fix log params

--- utils/mlflow_utils.py
+++ utils/mlflow_utils.py
@@ -6,6 +6,6 @@ def log_all_params(params):
     """dictの中にdictが入っている場合も書き出す"""
     for k, v in params.items():
         if isinstance(v, (dict, DictConfig)):
-            log_all_params({f"{k._k}": _v for _k, _v in v.items()})
+            log_all_params({f"{k}.{_k}": _v for _k, _v in v.items()})
         else:
             mlflow.log_param(k, v)
